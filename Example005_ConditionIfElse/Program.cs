52776422b4b0d885a0e19b720d996f1ea9a45ada
Anton <voodoo6991@gmail.com>
2022-10-01 13:12:34 +0300

    add Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "света")
+{
+    Console.WriteLine("Ура, это же Света!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
